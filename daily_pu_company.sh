d2e7bd575c6bc1cffc7278309ffea09958c62fdd
Ruchee <my@ruchee.com>
2014-03-30 23:47:06 +0800

    daily backup at home

---
+++ daily_pu_company.sh
@@ -0,0 +1,7 @@
+cd ~/vim
+git add --all && git cmm "daily backup at company" && git pu
+cd ~/mysite
+git add --all && git cmm "daily backup at company" && git pu
+cd ~/backup
+git add --all && git cmm "daily backup at company" && git pu
+cd ~
