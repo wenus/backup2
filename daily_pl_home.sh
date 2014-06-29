222908417dc270321e10cb707c8ca23c3cf6c6c2
Ruchee <my@ruchee.com>
2014-06-29 14:56:42 +0800

    backup

--- daily_pl_home.sh
+++ daily_pl_home.sh
@@ -1,7 +1,5 @@
 cd ~/vim
 git pl
-cd ~/mysite
-git pl
 cd ~/backup
 git pl
 cd ~
--- daily_pu_home.sh
+++ daily_pu_home.sh
@@ -1,7 +1,5 @@
 cd ~/vim
 git add --all && git cmm "daily backup at home" && git pu
-cd ~/mysite
-git add --all && git cmm "daily backup at home" && git pu
 cd ~/backup
 git add --all && git cmm "daily backup at home" && git pu
 cd ~
