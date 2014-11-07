5d69ba770ebbf07f659ee613a6aa9f3137baf0b8
Ruchee <my@ruchee.com>
2014-11-07 20:41:56 +0800

    daily backup at company

---
+++ make_tags_for_ruby.bat
@@ -0,0 +1,2 @@
+@ECHO OFF
+ctags -R --languages=ruby ./gems/actionmailer-4.1.7 ./gems/actionpack-4.1.7 ./gems/actionview-4.1.7 ./gems/activemodel-4.1.7 ./gems/activerecord-4.1.7 ./gems/activesupport-4.1.7 ./gems/railties-4.1.7
